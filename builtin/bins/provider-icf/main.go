6c0f9d3215e14798b94801fd0ad27bb66c9dd9af
Joseph Swaminathan <jswamina@cisco.com>
2016-04-14 14:08:46 -0700

    fix references

--- builtin/bins/provider-icf/main.go
+++ builtin/bins/provider-icf/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"cto-github.cisco.com/jswamina/kvs_infra/src/infra/log"
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/jswamina/terraform/icf"
+	"github.com/joeswaminathan/terraform/icf"
 )
 
 func main() {
-	log.StartLogger("icf-terraform", true)
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: icf.Provider,
 	})
